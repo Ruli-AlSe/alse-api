d6d4f92018592fafb2de574cf88ce461f836c328
Raul Almanza <ra.almanzaserrano@gmail.com>
2025-01-08 14:11:52 -0600

    Create all required code to pass tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  # validations
+  validates :email, :password_digest, :type, :age, presence: true
+  validates :email, uniqueness: true
+  validates :email, format: { with: /\A[\W+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :age, numericality: { only_integer: true, greater_than_or_equal_to: 18, less_than_or_equal_to: 100 }
 end
