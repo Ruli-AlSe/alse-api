0b3ac131d80a8cd289fca8837d5cc38ed959f74c
Raul Almanza <ra.almanzaserrano@gmail.com>
2025-01-08 04:01:12 -0600

    Generate user model and write tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validations of user model' do
+    subject { build(:user) }
+
+    it 'validate if email is present' do
+      should validate_presence_of(:email)
+    end
+
+    it 'validate if password is present' do
+      should validate_presence_of(:password_digest)
+    end
+
+    it 'validate if type is present' do
+      should validate_presence_of(:type)
+    end
+
+    it 'validate if age is present' do
+      should validate_presence_of(:age)
+    end
+
+    it 'validate if email is unique' do
+      should validate_uniqueness_of(:email)
+    end
+
+    it 'validate reject wrong emails' do
+      should_not allow_value('test').for(:email)
+    end
+
+    it 'validate age is an integer' do
+      should validate_numericality_of(:age).only_integer
+    end
+
+    it 'validate age is greater than or equal to 18' do
+      should validate_numericality_of(:age).is_greater_than_or_equal_to(18)
+    end
+
+    it 'validate age is lower than or equal to 100' do
+      should validate_numericality_of(:age).is_less_than_or_equal_to(100)
+    end
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,7 +40,7 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.include ApplicationHelper
-  config.include FactoryBot::syntax::Methods
+  config.include FactoryBot::Syntax::Methods
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
