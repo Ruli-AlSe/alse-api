3f13819bc5da838f2c2a36eeb061f8de77bfec84
Raul Almanza <ra.almanzaserrano@gmail.com>
2025-01-08 16:52:48 -0600

    create login method in users controller and its tests

---
+++ spec/controllers/users/login_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe V1::UsersController, type: :controller do
+  describe 'User login' do
+    let(:user) { create(:owner, password: '12345678') }
+
+    context 'with successful login' do
+      before do
+        post(:login, format: :json, params: { user: { email: user.email, password: '12345678' } })
+      end
+
+      context 'ok status' do
+        subject { response }
+
+        it { is_expected.to have_http_status(:ok) }
+      end
+
+      context 'correct login response' do
+        subject { payload_test }
+
+        it { is_expected.to include(:id, :email, :age, :store) }
+      end
+    end
+
+    context 'with unsuccessful login' do
+      before do
+        post(:login, format: :json, params: { user: { email: user.email, password: '' } })
+      end
+
+      context 'bad request status' do
+        subject { response }
+
+        it { is_expected.to have_http_status(:bad_request) }
+      end
+
+      context 'incorrect login response' do
+        subject { payload_test }
+
+        it { is_expected.to include(:errors) }
+      end
+    end
+  end
+end
--- spec/routing/user_spec.rb
+++ spec/routing/user_spec.rb
@@ -4,4 +4,8 @@ describe 'User routes' do
   it 'users register' do
     expect(post: '/v1/users').to route_to(format: :json, controller: 'v1/users', action: 'create')
   end
+
+  it 'users login' do
+    expect(post: '/v1/users/login').to route_to(format: :json, controller: 'v1/users', action: 'login')
+  end
 end
